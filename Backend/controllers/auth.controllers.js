693f218a5f5199dc38e92a313ec1b3f23e7a7136
Avishkar <avishkar1234567890@gmail.com>
2026-03-08 20:53:41 +0530

    feat: logout functionality implement

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 
 export async function signup (req, res) {
     try {
@@ -48,12 +49,15 @@ export async function signup (req, res) {
             image,
         });
 
+        generateTokenAndSetCookie(newUser._id, res);
         await newUser.save();
-
-        res.status(200).json({success: true, user: {
-            ...newUser._doc,
-            password: ""
-        }})
+        res.status(201).json({
+        success: true,
+         user: {
+        ...newUser._doc,
+        password: "",
+    }
+});
 
     } catch (error) {
         console.log("Error in signup controller", error.message);
@@ -66,5 +70,11 @@ export async function login(req, res) {
 }
 
 export async function logout(req, res) {
-    res.send("Logout route");
+    try {
+        res.clearCookie("jwt-netflix");
+        res.status(200).json({ success: true, message: "Logged Out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        req.status(500).json({ success: false, message: "Internal server error" });
+    }
 }
