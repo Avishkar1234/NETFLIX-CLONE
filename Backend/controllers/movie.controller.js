b88b5a8c85b75c21aafa162f40fc6dedfc40bf66
Avishkar <avishkar1234567890@gmail.com>
2026-03-10 00:04:04 +0530

    feat: add similar movies endpoint to fetch related films

--- Backend/controllers/movie.controller.js
+++ Backend/controllers/movie.controller.js
@@ -36,7 +36,18 @@ export async function getMovieDetails(req, res) {
         if(error.message.includes("404")) {
             return res.status(404).send(null);
         }
-
         res.status(500).json({ success: false, message: "Internal server error"});
     }
+}
+
+export async function getSimilarMovies(req, res) {
+
+    const { id } = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar`);
+        res.status(200).json({ success: true, similar: data.results })
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Internal Server Error"})
+    }
+
 }
