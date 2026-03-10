f07250b67dbe60d98b7f671c68102689bd2cbc98
Avishkar <avishkar1234567890@gmail.com>
2026-03-10 20:25:53 +0530

    feat: implement movie search functionality with error handling and search history update

--- Backend/controllers/search.controller.js
+++ Backend/controllers/search.controller.js
@@ -31,7 +31,32 @@ export async function searchPerson(req, res) {
 };
 
 export async function serachMovie(req, res) {
+    const { query } = req.params;
 
+    try {
+        const response = fetchFromTMDB(`https://api.themoviedb.org/3/search/movie?query=${query}&language=en-US&page=1`);
+
+        if(!response.results.length === 0) {
+            res.status(404).send(null);
+        }
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: {
+                searchHistory: {
+                    id: response.results[0].id,
+                    image: response.results[0].poster_path,
+                    title: response.results[0].title,
+                    searchType: "movie",
+                    createdAt: new Date(),
+                },
+            },
+        });
+
+        res.status(200).json({ success: true, content: response.results})
+    } catch (error) {
+        console.log("Error in serachPerson controller: ", error.message);
+        res.status(500).json({ success: false, message: "Internal Server error"});
+    }
 };
 
 export async function serachTv(req, res) {
