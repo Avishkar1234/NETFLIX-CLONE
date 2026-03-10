1a646d71c62794666ed0e2f9a706b73de4e4f841
Avishkar <avishkar1234567890@gmail.com>
2026-03-10 19:50:11 +0530

    feat: implement protectRoute middleware for secure API access

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.route.js";
@@ -6,15 +7,17 @@ import tvRoutes from "./routes/tv.routes.js";
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from './middleware/protectRoute.js';
 
 const app = express();
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse req.body object
+app.use(cookieParser()); 
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/movie", movieRoutes);
-app.use("/api/v1/tv", tvRoutes);
+app.use("/api/v1/movie",protectRoute ,movieRoutes);
+app.use("/api/v1/tv",protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
