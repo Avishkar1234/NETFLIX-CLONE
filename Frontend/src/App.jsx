de297a8e04f56398fc8feb317b60c9538bd47dc1
Avishkar <avishkar1234567890@gmail.com>
2026-03-12 21:26:27 +0530

    feat: restructure HomePage and add AuthScreen component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/LoginPage";
-import HomePage from "./pages/HomePage";
 import SignUp from "./pages/SignUpPage";
 
 function App() {
---
+++ Frontend/src/pages/home/HomeScreen.jsx
@@ -0,0 +1,8 @@
+
+const HomeScreen = () => {
+  return (
+    <div>This is HomeScreen</div>
+  )
+}
+
+export default HomeScreen
