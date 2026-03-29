5656a51b796355873658c38e6b4563ff05d939c9
Avishkar <avishkar1234567890@gmail.com>
2026-03-29 20:49:28 +0530

    feat: add WatchPage component for displaying movie trailers and similar content

---
+++ Frontend/src/pages/WatchPage.jsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { useContentStore } from '../store/content';
+import axios from 'axios';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
+import Navbar from '../../components/Navbar';
+
+const WatchPage = () => {
+    const { id } = useParams();
+    const [trailers, setTrailers] = useState([]);
+    const [currentTrailerIdx, setCurrentTrailerIdx] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [content, setContent] = useState({});
+    const [similarContent, setSimilarContent] = useState([]);
+    const { contentType } = useContentStore();
+
+    useEffect(() => {
+        const getTrailers = async () => {
+            try {
+                const res = await axios.get(`/api/v1/${contentType}/${id}/trailers`);
+                setTrailers(res.data.trailers);
+                console.log("Trailers: ", res.data.trailers);
+            } catch (error) {
+                if (error.message.includes('404')) {
+                    setTrailers([]);
+                }
+            }
+        }
+
+        getTrailers();
+    }, [contentType, id]);
+
+    useEffect(() => {
+        const getSimiarContent = async () => {
+            try {
+                const res = await axios.get(`/api/v1/${contentType}/${id}/similar`);
+                setSimilarContent(res.data.similar);
+            } catch (error) {
+                if (error.message.includes('404')) {
+                    setSimilarContent([]);
+                }
+            }
+        }
+        
+        getSimiarContent();
+    }, [contentType, id]);
+
+    useEffect(() => {
+        const getContentDetails = async () => {
+            try {
+                const res = await axios.get(`/api/v1/${contentType}/${id}/details`);
+                setContent(res.data.content);
+            } catch (error) {
+                if (error.message.includes('404')) {
+                    setContent(null);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+        
+        getContentDetails();
+    }, [contentType, id]);
+
+    const handleNext = () => {
+        if (currentTrailerIdx < trailers.length - 1) {
+            setCurrentTrailerIdx(currentTrailerIdx + 1)
+        }
+    }
+    const handlePrev = () => {
+        if (currentTrailerIdx > 0) {
+            setCurrentTrailerIdx(currentTrailerIdx - 1)
+        }
+    }
+
+  return <div className='bg-black min-h-screen text-white'>
+    <div className='mx-auto container px-4 py-8 h-full'>
+        <Navbar />
+
+        {trailers.length > 0 && (
+            <div className='flex justify-between items-center mb-4'>
+                <button className={`
+                        bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${currentTrailerIdx === 0 ? 'opacity-50 cursor-not-allowed' : ""
+                        }`}
+                            disabled={currentTrailerIdx === 0}
+                            onClick={handlePrev}
+                        >
+                    <ChevronLeft size={24}
+                    />
+                </button>
+
+                <button className={`
+                        bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${currentTrailerIdx === trailers.length - 1 ? 'opacity-50 cursor-not-allowed' : ""
+                        }`}
+                            disabled={currentTrailerIdx === trailers.length - 1}
+                            onClick={handleNext}
+                        >
+                    <ChevronRight size={24} 
+                    />
+                </button>
+            </div>
+        )}
+
+        <div className='aspect-video mb-8 p-2 sm:px-10 md:px-32'>
+            {trailers.length > 0 && (
+
+            )};
+        </div>
+    </div>
+  </div>
+}
+
+export default WatchPage
