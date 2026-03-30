e5e8dd5ab07ac97f8a356b39cbd065e5b0d2f61c
Avishkar <avishkar1234567890@gmail.com>
2026-03-30 20:51:16 +0530

    feat: add SearchHistoryPage and NotFoundPage components, update routing and styles

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,8 @@ import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import WatchPage from "./pages/WatchPage";
 import SearchPage from "./pages/SearchPage";
+import SearchHistoryPage from "./pages/SearchHistoryPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const { user, isCheckingAuth, authCheck } = useAuthStore();
@@ -35,6 +37,8 @@ function App() {
         <Route path='/signup' element={!user ? <SignUp/> : <Navigate to={"/"} />} />
         <Route path='/watch/:id' element={user ? <WatchPage /> : <Navigate to={"/login"} />} />
         <Route path='/search' element={user ? <SearchPage /> : <Navigate to={"/login"} />} />
+        <Route path='/history' element={user ? <SearchHistoryPage /> : <Navigate to={"/login"} />} />
+        <Route path='/*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
 
